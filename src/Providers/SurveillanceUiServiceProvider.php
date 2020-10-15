38ddf71e7044c98eea4f60ac059c3006dea8ecc6
neelkanthk <me.neelkanth@gmail.com>
2020-10-15 14:58:47 +0530

    Initial structure

---
+++ src/Providers/SurveillanceUiServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Neelkanth\Laravel\SurveillanceUi\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SurveillanceUiServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
