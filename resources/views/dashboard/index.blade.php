b45dc5d982e8540cffb72ea255b3c8064a983f76
neelkanthk <me.neelkanth@gmail.com>
2020-10-18 20:01:05 +0530

    language files in dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -23,22 +23,21 @@
         <!-- Project Card Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-warning">On Surveillance - 20</h6>
+                <h6 class="m-0 font-weight-bold text-dark">{{ __('surveillance-ui::app.surveillance_status.enable') }}</h6>
             </div>
             <div class="card-body">
-                <h4 class="small font-weight-bold">IP Address<span class="float-right">20</span></h4>
+                <h4 class="small font-weight-bold">{{ __('surveillance-ui::app.surveillance_types.ip') }}<span class="float-right">20</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-success" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <h4 class="small font-weight-bold">Fingerprint<span class="float-right">40</span></h4>
+                <h4 class="small font-weight-bold">{{ __('surveillance-ui::app.surveillance_types.fingerprint') }}<span class="float-right">40</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-info" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <h4 class="small font-weight-bold">User ID<span class="float-right">60</span></h4>
+                <h4 class="small font-weight-bold">{{ __('surveillance-ui::app.surveillance_types.user_id') }}<span class="float-right">60</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-
             </div>
         </div>
     </div>
@@ -49,22 +48,21 @@
         <!-- Project Card Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-danger">Blocked - 10</h6>
+                <h6 class="m-0 font-weight-bold text-dark">{{ __('surveillance-ui::app.surveillance_status.block') }}</h6>
             </div>
             <div class="card-body">
-                <h4 class="small font-weight-bold">IP Address<span class="float-right">20</span></h4>
+                <h4 class="small font-weight-bold">{{ __('surveillance-ui::app.surveillance_types.ip') }}<span class="float-right">20</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-success" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <h4 class="small font-weight-bold">Fingerprint<span class="float-right">40</span></h4>
+                <h4 class="small font-weight-bold">{{ __('surveillance-ui::app.surveillance_types.fingerprint') }}<span class="float-right">40</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-info" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <h4 class="small font-weight-bold">User ID<span class="float-right">60</span></h4>
+                <h4 class="small font-weight-bold">{{ __('surveillance-ui::app.surveillance_types.user_id') }}<span class="float-right">60</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-
             </div>
         </div>
     </div>
